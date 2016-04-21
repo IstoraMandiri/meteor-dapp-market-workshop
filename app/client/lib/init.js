2e89ef8d02cf98c0c5bc91852440dca55cc96d56
Chris Hitchcott <hitchcott@gmail.com>
2016-04-21 18:58:29 +0100

    3. Market Smart Contract

--- app/client/lib/init.js
+++ app/client/lib/init.js
@@ -6,3 +6,8 @@ import Web3 from 'web3'
 window.web3 = new Web3()
 // set the provider to our local dev node
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
+
+// use coinbase as `defaultAccount`
+Meteor.startup(function () {
+  web3.eth.defaultAccount = web3.eth.coinbase
+})
