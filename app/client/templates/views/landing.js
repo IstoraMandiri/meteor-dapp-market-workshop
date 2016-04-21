5e6613a458f1c622eb6a29d564701cb2fa1189e0
Chris Hitchcott <hitchcott@gmail.com>
2016-04-21 19:33:07 +0100

    7. Create a Reusable Deploy Step

--- app/client/templates/views/landing.js
+++ app/client/templates/views/landing.js
@@ -1,26 +1,13 @@
 Template.landing.events({
   'click .new-market': function (e, tmpl) {
-    // before starting let's update the UI to show a loading bar
-    TemplateVar.set('deploying', true)
-    // deploy the contract
-    Market.new({
-      // send bytecode in deploy step
-      data: Market.bytecode,
-      // max-out gas for now to avoid errors
-      gas: 3000000
-    }, function (err, contract) {
-      // this callback fires multiple times
-      // alert user if there's a problem
-      if (err) {
-        // update the template data to hide the
-        TemplateVar.set(tmpl, 'deploying', false)
-        window.alert(err)
-      }
-      // if there is no error we shold get a second callback
-      // the `address` property will exist after the contract is mined
-      if (contract.address) {
-        FlowRouter.go('market', {address: contract.address})
-      }
+    app.deployContract({
+      tmpl: tmpl,
+      template: 'marketInfoForm',
+      title: 'Create a new market',
+      contract: Market
+    }, function (err, address) {
+      if (err) { throw err }
+      FlowRouter.go('market', {address: address})
     })
   },
   'submit form': function (e, tmpl) {
